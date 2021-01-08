e2e908f42adb7bcdc5d613645bd51a52fad93f4a
Will Carter <will.carter@fergusllc.com>
2021-01-08 14:06:01 -0700

    remove REDUX_DEVTOOLS

--- src/store.js
+++ src/store.js
@@ -10,9 +10,10 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
   )
-);
+)
 
-export default store;
+export default store
+
+// ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
